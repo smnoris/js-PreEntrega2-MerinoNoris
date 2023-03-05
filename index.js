cdcbd4c26a169740fe259fed26cd5c526bfb3a10
Sebas Merino <sebas.mernor@gmail.com>
2023-03-04 22:37:33 -0300

    index

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 12; i++) {
     alert("lectura n°" + i)
 
 let voltaje = prompt("Ingrese voltaje");
     alert("El voltaje es " + voltaje);
     
+
 let amperaje = prompt("Ingrese corriente");
     alert("La corriente es " + amperaje);
     
 let resistencia = prompt("Ingrese resistencia");
     alert("La resistencia es " + resistencia);
+
+if (voltaje === null ) {
+    
+}
+
 }  
+
+
 
 const lectura = alert('');
 console.log("Voltaje: "+ voltaje + " volts");
