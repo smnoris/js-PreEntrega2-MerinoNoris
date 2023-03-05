f11b561dec2084f16f20e86aabc0942c91727301
Sebas Merino <sebas.mernor@gmail.com>
2023-03-04 21:54:09 -0300

    subida index y js

---
+++ index.js
@@ -0,0 +1,18 @@
+for (let i = 0; i < 10; i++) {
+    alert("lectura n°" + i)
+
+let voltaje = prompt("Ingrese voltaje");
+    alert("El voltaje es " + voltaje);
+    
+let amperaje = prompt("Ingrese corriente");
+    alert("La corriente es " + amperaje);
+    
+let resistencia = prompt("Ingrese resistencia");
+    alert("La resistencia es " + resistencia);
+}  
+
+const lectura = alert('');
+console.log("Voltaje: "+ voltaje + " volts");
+console.log("Corriente: "+ amperaje + " amperes");
+console.log("Resistencia: "+ resistencia + " ohms");
+
